9f6d42c04f06b73033d2b37163748573cc3d6bfc
madannaik <naikmadan49@gmail.com>
2021-08-11 19:50:10 +0530

    second commit

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -7,6 +7,10 @@ export const GlobalStyles = createGlobalStyle`
     .clock{
         background-color:${({ theme }) => theme.body};
 
+        &__circle {
+            box-shadow:${({ theme }) => theme.boxShadow};
+        }
+
     &__digi{
         color : ${({ theme }) => theme.text};
     }
--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -1,9 +1,11 @@
 export const lightTheme = {
   body: "#fff",
   text: "#000",
+  boxShadow: "36px 14px 30px 9px #172f6a99,-44px -7px 40px 15px rgba(0, 0, 0, 0.1)"
 };
 
 export const darkTheme = {
   body: "#152552",
   text: "#fff",
+  boxShadow: "36px 14px 30px 9px #172f6a99,-44px -7px 40px 15px rgba(0, 0, 0, 0.25)"
 };
